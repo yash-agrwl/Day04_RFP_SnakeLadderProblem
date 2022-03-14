2d9e8e21c8d7e402b0c6512e0a7c06556571be26
yash-agrwl <agarwalyash7012@gmail.com>
2022-03-14 13:47:29 +0530

    added empty project file with welcome message

---
+++ SnakeAndLadder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SnakeAndLadder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Computation Of Snake And Ladder Problem\n");
+        }
+    }
+}
