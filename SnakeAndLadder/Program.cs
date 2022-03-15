44ca4bc5c9c90dee2bb34c7f5814d041e440a54c
yash-agrwl <agarwalyash7012@gmail.com>
2022-03-15 18:09:41 +0530

    added uc1: Initialised Player1 with start Position

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -4,9 +4,13 @@ namespace SnakeAndLadder
 {
     internal class Program
     {
+        static int Start = 0;
+        int Position = Start;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Computation Of Snake And Ladder Problem\n");
+            Program Player1 = new Program();
         }
     }
 }
