8b1227c170decc1f363652a8be2006defb41ce38
yash-agrwl <agarwalyash7012@gmail.com>
2022-03-15 13:11:02 +0530

    added uc3: option check method for the player

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -7,12 +7,26 @@ namespace SnakeAndLadder
 
         static int Start = 0;
         int Position = Start;
+        const int NoPlay = 0, Ladder = 1, Snake = 2;
 
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Computation Of Snake And Ladder Problem\n");
             Program Player1 = new Program();
             int die = Player1.RollDie();
+            int option = Player1.CheckOption();
+            switch (option)
+            {
+                case Ladder:    
+                    Player1.Position += die;
+                    break;
+                case Snake:
+                    Player1.Position -= die;
+                    break;
+                default:
+                    break;
+            }
+
         }
 
         public int RollDie()
@@ -20,6 +34,13 @@ namespace SnakeAndLadder
             Random rand = new Random();
             int die = rand.Next(1, 7);
             return die;
+        }
+
+        public int CheckOption()
+        {
+            Random rand = new Random();
+            int option = rand.Next(0,3);
+            return option;
         }
     }
 }
