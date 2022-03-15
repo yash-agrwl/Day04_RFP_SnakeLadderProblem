11b55c409e35f8c5ca8f0dbccb9655c2ac29ff75
yash-agrwl <agarwalyash7012@gmail.com>
2022-03-15 12:58:48 +0530

    added uc2: RollDie to get number between 1 to 6

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -4,9 +4,22 @@ namespace SnakeAndLadder
 {
     internal class Program
     {
+
+        static int Start = 0;
+        int Position = Start;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Computation Of Snake And Ladder Problem\n");
+            Program Player1 = new Program();
+            int die = Player1.RollDie();
+        }
+
+        public int RollDie()
+        {
+            Random rand = new Random();
+            int die = rand.Next(1, 7);
+            return die;
         }
     }
 }
